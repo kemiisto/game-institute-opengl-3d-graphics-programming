836785eff57164b1b2e60228c099b62e24f6ca2e
kemiisto <kemiisto@gmail.com>
2023-04-19 21:45:46 +0300

    build: switch to tinygl

--- 03/01.cpp
+++ 03/01.cpp
@@ -1,52 +1,21 @@
 /**
  * Creates a basic resizeable window and OpenGL 3.3 context using GLFW.
  */
+#include "main.h"
+#include <tinygl/tinygl.h>
 #include <iostream>
 
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-const char* windowTitle = "Introduction to Modern OpenGL - Hello Window 1";
-const int windowWidth = 800;
-const int windowHeight = 600;
-
-int main()
+class Window final : public tinygl::Window
 {
-    if (!glfwInit()) {
-        std::cerr << "GLFW initialization failed" << std::endl;
-        return EXIT_FAILURE;
-    }
+public:
+    using tinygl::Window::Window;
+    void draw() override;
+};
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-    GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, windowTitle, nullptr, nullptr);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return EXIT_FAILURE;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "Failed to initialize GLEW" << std::endl;
-        glfwTerminate();
-        return EXIT_FAILURE;
-    }
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-
-        glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-    }
-
-    glfwTerminate();
-    return EXIT_SUCCESS;
+void Window::draw()
+{
+    glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
+
+MAIN
--- 03/02.cpp
+++ 03/02.cpp
@@ -1,74 +1,42 @@
 /**
  * Creates a basic resizeable window and OpenGL 3.3 context using GLFW.
- * Creates onKey keyboard handler callback and sets the Esc key to exit the program.
+ * Creates keyboard handler callback and sets the Esc key to exit the program.
  * Creates FPS counter and displays in app title bar.
  */
+#include "main.h"
+#include <tinygl/tinygl.h>
 #include <iostream>
 #include <sstream>
 
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+const char* windowTitle = NAME;
 
-const char* windowTitle = "Introduction to Modern OpenGL - Hello Window 2";
-const int windowWidth = 800;
-const int windowHeight = 600;
-
-void onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
-void showFPS(GLFWwindow* window);
-
-int main()
+class Window final : public tinygl::Window
 {
-    if (!glfwInit()) {
-        std::cerr << "GLFW initialization failed" << std::endl;
-        return EXIT_FAILURE;
-    }
+public:
+    using tinygl::Window::Window;
+    void init() override;
+    void draw() override;
+private:
+    void showFPS();
+};
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-    GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, windowTitle, nullptr, nullptr);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return EXIT_FAILURE;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "Failed to initialize GLEW" << std::endl;
-        glfwTerminate();
-        return EXIT_FAILURE;
-    }
-
-    glfwSetKeyCallback(window, onKey);
-
-    while (!glfwWindowShouldClose(window)) {
-        showFPS(window);
-
-        glfwPollEvents();
-
-        glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-    }
-
-    glfwTerminate();
-    return EXIT_SUCCESS;
+void Window::init()
+{
+     setKeyCallback([this](tinygl::keyboard::Key key, int /*scancode*/, tinygl::input::Action action, tinygl::input::Modifier /*mods*/) {
+        if (key == tinygl::keyboard::Key::Escape && action == tinygl::input::Action::Press) {
+            setShouldClose(true);
+        }
+    });
 }
 
-void onKey(GLFWwindow* window, int key, int scancode, int action, int mode)
+void Window::draw()
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GL_TRUE);
-    }
+    showFPS();
+    glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
 
-void showFPS(GLFWwindow* window) {
+void Window::showFPS() {
     static double previousSeconds = 0.0;
     static int frameCount = 0;
 
@@ -84,7 +52,9 @@ void showFPS(GLFWwindow* window) {
         std::ostringstream oss;
         oss.precision(3);
         oss << std::fixed << windowTitle << "    " << "FPS: " << fps << "    " << "Frame Time: " << frameTime << " (ms)";
-        glfwSetWindowTitle(window, oss.str().c_str());
+        setTitle(oss.str());
         frameCount = 0;
     }
 }
+
+MAIN
